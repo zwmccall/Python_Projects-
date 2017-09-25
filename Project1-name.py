f3f32668e246afedceb8ad492b0b010f4ee75ff7
Syed Marwan Jamal <31986276+Marwan2001@users.noreply.github.com>
2017-09-25 16:04:43 +0530

    Create Project1-name.py

---
+++ Project1-name.py
@@ -0,0 +1,11 @@
+# Author : Syed Marwan Jamal.
+# This program asks for user name,
+# the length of the name is also returned.
+# Please give your feedback on the program and way to improve it.
+
+
+print('Hello')
+user_name = input('What is your name?\n') # Asks for user name. 
+print('It\'s nice to meet you', user_name)
+print('Length of your name is:')
+print(len(user_name)) # Returns name's length
