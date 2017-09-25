e92c0d2888fbb52aa90b7c0cea9ff75d33e881e8
Syed Marwan Jamal <31986276+Marwan2001@users.noreply.github.com>
2017-09-25 16:18:49 +0530

    Project 2

---
+++ Project2-age.py
@@ -0,0 +1,11 @@
+# Author : Syed Marwan Jamal.
+# This program asks for user age,
+# returns their age in x years.
+# Please give your feedback on the program and way to improve it.
+
+
+print('Hello')
+user_age = input('What is your age? \n')
+print('Oh so your age is', user_age)
+years = input('Enter a numberto know your age after that many years \n')
+print('You will be', int(years)+int(user_age), 'in', years ,'years')
