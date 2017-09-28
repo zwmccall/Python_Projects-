a718ac66164ba0142d852eb220ad73904ba896c6
Yogitha Polavarapu <yogitha.vja@gmail.com>
2017-09-28 11:06:04 +0530

    Update Project3-weeks_and_days(function).py
    typo error

--- Project3-weeks_and_days(function).py
+++ Project3-weeks_and_days(function).py
@@ -1,6 +1,6 @@
 #This is a function.
 def readable_timedelta(days):
-   """To get the number of weeks and days in given nuber of days"""
+   """To get the number of weeks and days in given number of days"""
    number_of_weeks = days // 7 #To get number of weeks
    number_of_days = days % 7 # To get number of days
    return('{} week(s) and {} day(s)'.format(number_of_weeks, number_of_days))
